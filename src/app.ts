a6b34761eda812b066fc939da9e6bab50a724a31
yonasketema <yonask.kitaw@gmail.com>
2024-04-07 23:24:22 +0300

    config: cookie, cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import path from "path";
+import cookieParser from "cookie-parser";
 
 import messageRouter from "./routes/messageRoute";
 import userRouter from "./routes/userRoute";
 
 const app = express();
+
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Replace with the allowed origin(s)
+    credentials: true, // Allow credentials in cross-origin requests
+  })
+);
 
 app.use(morgan("dev"));
 app.use(express.json());
